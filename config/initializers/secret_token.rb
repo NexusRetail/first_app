92265ac855aad0178c8fe23832dfab1d743f9583
David Fisher <dfisher@nexusretail.com>
2012-02-18 21:49:07 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'de55ebe60fcc6f398a3a1f8d2e7303e78918d8aa490fdb095aba898e28e218862536c350ae8b93e9a5baaf87acb1e59072cb1e13316ba301a2f2002b6b11de6f'
